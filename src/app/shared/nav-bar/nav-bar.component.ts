044cc022116915cf91256bfeaaaf026a5d124a7a
Meschack <meshachgbewezoun@gmail.com>
2023-09-01 16:43:13 +0000

    Petites corrections...

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigateByUrl('/');
+  }
 }
